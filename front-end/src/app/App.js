c9784c0a6ebbd04c18129ba511639ae0d9ab2a28
CalebLangbroek <34527287+CalebLangbroek@users.noreply.github.com>
2021-03-27 00:32:47 -0700

    Testing shared package

--- front-end/src/app/App.js
+++ front-end/src/app/App.js
@@ -1,7 +1,16 @@
+import React, { Component } from 'react';
 import './App.css';
+import { Constants, Note } from 'shared';
 
-function App() {
-    return <div>Hello</div>;
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {Constants.API_BASE_URL}
+                <div>{new Note(12, "test", "test").id}</div>
+            </div>
+        );
+    }
 }
 
 export default App;
